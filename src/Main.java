7961fba07a518630436522b21f6066788ef58100
illumiMati <illumiMati@github.com>
2025-05-20 16:55:04 +0200

    add multi error handling

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static int Add(String numbers) {
         List<Integer> intList = new ArrayList<>();
         List<Integer> negativeNumbers = new ArrayList<>();
+        List<String> errorMessages = new ArrayList<>();
         int sum = 0;
 
         if(numbers.isEmpty()) return sum;
@@ -19,15 +20,20 @@ public class Main {
         if(numbers.startsWith("//")) {
             int newLineIndex = numbers.indexOf("\n");
 
-            if(newLineIndex == -1) throw new IllegalArgumentException("Illegal format - no \n after separator declaration");
-
-            delimiter = numbers.substring(2, newLineIndex);
-            numbersPart = numbers.substring(newLineIndex + 1);
+            if(newLineIndex == -1) {
+                errorMessages.add("Illegal format - no \\n after separator declaration");
+                numbersPart = "";
+            } else {
+                delimiter = numbers.substring(2, newLineIndex);
+                numbersPart = numbers.substring(newLineIndex + 1);
+            }
         } else {
             numbersPart = numbers.replace("\n", delimiter);
         }
 
-        if(numbersPart.endsWith(delimiter)) throw new IllegalArgumentException("Error: Separators are not allowed at the end of String");
+        if(numbersPart.endsWith(delimiter)) {
+            errorMessages.add("Separators are not allowed at the end of String");
+        }
 
 
         int position = 0;
@@ -38,11 +44,8 @@ public class Main {
                 String value = currentNumber.toString();
                 if(!value.isEmpty()) {
                     int number = Integer.parseInt(value);
-                    if(number < 0) {
-                        negativeNumbers.add(number);
-                    } else {
-                        intList.add(number);
-                    }
+                    if (number < 0) negativeNumbers.add(number);
+                    else intList.add(number);
                     currentNumber.setLength(0);
                 }
                 position += delimiter.length();
@@ -50,22 +53,24 @@ public class Main {
                 currentNumber.append(numbersPart.charAt(position));
                 position++;
             } else {
-                throw new IllegalArgumentException("'" + delimiter + "' expected but '" + numbersPart.charAt(position) + "' found at position " + position);
+                errorMessages.add("'" + delimiter + "' expected but '" + numbersPart.charAt(position) + "' found at position " + position + ".");
+                currentNumber.setLength(0);
+                position++;
             }
         }
 
         if(!currentNumber.toString().trim().isEmpty()) {
             int number = Integer.parseInt(currentNumber.toString());
-            if(number < 0) {
-                negativeNumbers.add(number);
-            } else {
-                intList.add(number);
-            }
+            if (number < 0) negativeNumbers.add(number);
+            else intList.add(number);
         }
 
         if(!negativeNumbers.isEmpty()) {
-            throw new IllegalArgumentException("Negative number(s) not allowed: " +
-                    negativeNumbers.toString().replaceAll("[\\[\\]]", ""));
+            errorMessages.add("Negative number(s) not allowed: " + negativeNumbers.toString().replaceAll("[\\[\\]]", ""));
+        }
+
+        if(!errorMessages.isEmpty()) {
+            throw new IllegalArgumentException(String.join("\n", errorMessages));
         }
 
         for(int i : intList) {
