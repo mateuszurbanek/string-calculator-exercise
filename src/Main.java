60a3713ed01f288277d794642e90f7b8d53c2282
illumiMati <illumiMati@github.com>
2025-05-20 16:12:10 +0200

    add negative numbers error handling

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static int Add(String numbers) {
         List<Integer> intList = new ArrayList<>();
+        List<Integer> negativeNumbers = new ArrayList<>();
         int sum = 0;
 
         if(numbers.isEmpty()) return sum;
@@ -36,11 +37,16 @@ public class Main {
             if(numbersPart.startsWith(delimiter, position)) {
                 String value = currentNumber.toString();
                 if(!value.isEmpty()) {
-                    intList.add(Integer.parseInt(value));
+                    int number = Integer.parseInt(value);
+                    if(number < 0) {
+                        negativeNumbers.add(number);
+                    } else {
+                        intList.add(number);
+                    }
                     currentNumber.setLength(0);
                 }
                 position += delimiter.length();
-            } else if(Character.isDigit(numbersPart.charAt(position))) {
+            } else if(Character.isDigit(numbersPart.charAt(position)) || numbersPart.charAt(position) == '-') {
                 currentNumber.append(numbersPart.charAt(position));
                 position++;
             } else {
@@ -49,7 +55,17 @@ public class Main {
         }
 
         if(!currentNumber.toString().trim().isEmpty()) {
-            intList.add(Integer.parseInt(currentNumber.toString()));
+            int number = Integer.parseInt(currentNumber.toString());
+            if(number < 0) {
+                negativeNumbers.add(number);
+            } else {
+                intList.add(number);
+            }
+        }
+
+        if(!negativeNumbers.isEmpty()) {
+            throw new IllegalArgumentException("Negative number(s) not allowed: " +
+                    negativeNumbers.toString().replaceAll("[\\[\\]]", ""));
         }
 
         for(int i : intList) {
