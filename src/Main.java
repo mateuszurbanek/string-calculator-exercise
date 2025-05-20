f30f8345705af70ec8bda9afd3e3730dd8716946
illumiMati <illumiMati@github.com>
2025-05-20 14:15:45 +0200

    implement basic sum calculation and handle newlines as separators

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    public static int Add(String numbers) {
+        if(numbers.isEmpty()) return 0;
+
+        String normalizedNumbers = numbers.replace("\n", ",");
+        String[] numbersToSum = normalizedNumbers.split(",");
+        List<Integer> intList = new ArrayList<>();
+        int sum = 0;
+
+        for(String number : numbersToSum) {
+            if(!number.trim().isEmpty()) {
+                intList.add(Integer.parseInt(number.trim()));
+            }
+        }
+
+        for(int i : intList) {
+            sum += i;
+        }
+
+
+        return sum;
     }
 }
