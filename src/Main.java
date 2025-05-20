6654612cc0f24690e0746bb0fe1bfc86971b9f48
illumiMati <illumiMati@github.com>
2025-05-20 15:55:16 +0200

    add different delimiters handling

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,49 @@ public class Main {
     }
 
     public static int Add(String numbers) {
-        if(numbers.endsWith(",")) throw new IllegalArgumentException("Error: Separators are not allowed at the end of String");
-        if(numbers.isEmpty()) return 0;
-
-        String normalizedNumbers = numbers.replace("\n", ",");
-        String[] numbersToSum = normalizedNumbers.split(",");
         List<Integer> intList = new ArrayList<>();
         int sum = 0;
 
-        for(String number : numbersToSum) {
-            if(!number.trim().isEmpty()) {
-                intList.add(Integer.parseInt(number.trim()));
+        if(numbers.isEmpty()) return sum;
+
+        String delimiter = ",";
+        String numbersPart = numbers;
+
+        if(numbers.startsWith("//")) {
+            int newLineIndex = numbers.indexOf("\n");
+
+            if(newLineIndex == -1) throw new IllegalArgumentException("Illegal format - no \n after separator declaration");
+
+            delimiter = numbers.substring(2, newLineIndex);
+            numbersPart = numbers.substring(newLineIndex + 1);
+        } else {
+            numbersPart = numbers.replace("\n", delimiter);
+        }
+
+        if(numbersPart.endsWith(delimiter)) throw new IllegalArgumentException("Error: Separators are not allowed at the end of String");
+
+
+        int position = 0;
+        StringBuilder currentNumber = new StringBuilder();
+
+        while(position < numbersPart.length()) {
+            if(numbersPart.startsWith(delimiter, position)) {
+                String value = currentNumber.toString();
+                if(!value.isEmpty()) {
+                    intList.add(Integer.parseInt(value));
+                    currentNumber.setLength(0);
+                }
+                position += delimiter.length();
+            } else if(Character.isDigit(numbersPart.charAt(position))) {
+                currentNumber.append(numbersPart.charAt(position));
+                position++;
+            } else {
+                throw new IllegalArgumentException("'" + delimiter + "' expected but '" + numbersPart.charAt(position) + "' found at position " + position);
             }
+        }
+
+        if(!currentNumber.toString().trim().isEmpty()) {
+            intList.add(Integer.parseInt(currentNumber.toString()));
         }
 
         for(int i : intList) {
